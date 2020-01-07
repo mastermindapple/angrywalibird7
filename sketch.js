9b219168b34c662e1e4dfe9c26cfadbe830a74fb
Janav <dhingrajanavllc@gmail.com>
2020-01-07 20:14:55 +0530

    backgound change based on time

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var score = 0;
 var gameState = "onSling";
+var bg = "sprites/bg.png";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   getBackgroundImage();
 }
 
 function setup(){
@@ -45,18 +46,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score: "+ score,width-300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -83,6 +92,19 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+       slingshot.attach(bird.body);
     }
 }
+ async function getBackgroundImage(){
+   var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+   var responseJson = await response.json();
+   var dateTime = responseJson.datetime;
+   var hour = dateTime.slice(11,13);
+   if(hour>=06&&hour<19){
+       bg = "sprites/bg.png";
+   }
+   else{
+       bg = "sprites/bg2.jpg";
+   }
+   backgroundImg = loadImage(bg);
+ }
